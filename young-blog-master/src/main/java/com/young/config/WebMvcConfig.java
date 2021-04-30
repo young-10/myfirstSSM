c61fff77b84bfb8ba126bd03fe720b4eb6af2a96
young <18895678331@163.com>
2021-04-30 14:55:27 +0800

    commit-01

--- young-blog-master/src/main/java/com/young/config/WebMvcConfig.java
+++ young-blog-master/src/main/java/com/young/config/WebMvcConfig.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    //
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/admin/get/blogs").setViewName("admin/blogs");
